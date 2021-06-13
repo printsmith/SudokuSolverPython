ab9ae91c8a388ab019c5311a58e28072e61844e6
Tyler <27779354+printsmith@users.noreply.github.com>
2021-06-13 09:42:42 -0400

    Add isValid and SudokuSolver
    Begin developing algorithm to check validity

--- SudokuSolverText.py
+++ SudokuSolverText.py
@@ -16,13 +16,14 @@ grid = [
     [0,4,9,2,0,6,0,0,7],
 ]
 
+
 # Function prints Sudoku board with grid lines
 def print_board(board):
 
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
             print("----------------------")
-        for j in range(len(board)):
+        for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
                 print("| ",end="")
             if j == 8 and j != 0:
@@ -33,29 +34,52 @@ def print_board(board):
 # Print initial Sudoku grid
 print_board(grid)
 
+# Finds empty squares in board
+def find_empty(board):
+
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return(i, j) # returns the row and column of empty squares
+
+def SudokuSolver(board):
+
+    # Check if board is alrady solved, otherwise find first empty position to begin solving
+    if not find_empty(board):
+        return True
+    else:
+        find = find_empty(board)
+
+    # Run through numbers 1-9 and check validity 
+    for i in range(1,10):
+        # Pass in board, num and empty space coordinates (find) to isValid
+        if isValid(board, i, find):
+            board[find[0]][find[1]] = i
+    
+    print_board(board)
 
 
+# Checks if current number is already in row. 
+# num is a numbr 1-9 being tested, pos is the current position in the sudoku grid found via isValid
+def isValid(board, num, pos):
 
 
+    # pos[0] = row of test point
+    row = pos[0]
+    # pos[1] = column of test point
+    col = pos[1]
 
+    # Check row
+    for i in range(len(board[0])):
+        print(board[row][i])
+        if board[row][i] == num:
+            return False
+    
+    # Check column
+    # for i in range(len(board[0])):
+    #     if board[i][col] == num:
+    #         return False
 
+    return True
 
-# for i in range (len(board)):
-#     if i % 3 and i != 0:
-#         print("------------------------")
-
-#     for j in range(len(board[0])):
-#         if j % 3 == 0 and j != 0:
-#             print(" | ", end="")
-            
-#             if j == 8:
-#                 print(board[i][j])
-#             else
-#                 print(str(board[i][j]) + " ")
-
-# Generate 9x9 grid
-N = 9 
-
-#for i in range N:
-
-
+SudokuSolver(grid)
